36bba6b2c038a7a16fbd4ffa7cba8a7e4df94887
Jody Prinsloo <jprinsloo@my.bcit.ca>
2022-01-11 18:21:02 -0800

    completed basic animal card comp

--- exotico/comps/AnimalCard.js
+++ exotico/comps/AnimalCard.js
@@ -4,18 +4,42 @@ import React from "react";
 const CardContainer = styled.div`
 display: flex;
 flex-direction: column;
-width: 10%;
-height: 300px;
+justify-content: space-between;
+align-items: center;
+text-align: center;
+color: #fff;
+width: 10vw;
+height: 30vh;
 background-color: #a36abb;
-border-radius: 25px;
+border-radius: 15px;
 overflow: hidden;
 `;
 
-const AnimalImg = styled.img``;
+const AnimalImg = styled.img`
+display: flex;
+align-self: flex-start;
+width: 100%;
+`;
 
-const AnimalCardUI = () => {
+
+const AnimalCardUI = ({
+
+    name="Jake",
+    type="Finger Monkey",
+    age="5 months old"
+})=> {
     return <CardContainer>
         <AnimalImg src="https://placekitten.com/50/50" />
+        {/* <AnimalDescCont>
+            <AnimalName>{name}</AnimalName>
+            <AnimalType>{type}</AnimalType>
+            <AnimalAge>{age}</AnimalAge>
+        </AnimalDescCont> */}
+        {name}
+        <br/>
+        {type}
+        <br/>
+        {age}
     </CardContainer>
 }
 
--- exotico/pages/jodyTest.js
+++ exotico/pages/jodyTest.js
@@ -1,8 +1,17 @@
 import AnimalCardUI from "../comps/AnimalCard";
 import React from "react";
+import styled from "styled-components";
+
+const MainCont = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+`;
 
 export default function jodyTest() {
     return (
-        <AnimalCardUI />
+        <MainCont>
+            <AnimalCardUI />
+        </MainCont>
     )
 }
