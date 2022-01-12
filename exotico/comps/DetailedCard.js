590c7f8b7d67136a638cfe8ca546596714bb60fb
Ibjyot Gill <ibjyot17@gmail.com>
2022-01-11 18:55:51 -0800

    detailed card

--- exotico/comps/DetailedCard.js
+++ exotico/comps/DetailedCard.js
@@ -5,29 +5,71 @@ const Container = styled.div`
     width: 1200px;
     height: 620px;
     background-color: #9365A6;
+    border-radius: 25px;
+    display:flex;
 `;
 
-const Paragraph = styled.p`
-    font-size: 14px;
+const FImg = styled.img`
+    width: 480px;
+    height: 620px;
+`;
 
+const SCont = styled.div`
+    margin: 50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`;
+
+const Paras = styled.p`
+    font-size: 36px;
+    font-weight: bold;
+    color: #fff;
+`;
+
+const Para = styled.p`
+    font-size: 20px;
+    color: #fff;
+`;
+
+const TCont = styled.div`
+    margin: 100px;
+    display: flex;
+    justify-content: space-around;
 `;
 
 const DetailedCard = ({
-    animal="work",
-    type="",
-    age="",
-    country="",
-    vstatus=""
+    animal="Kurama",
+    type="Maned Wolf",
+    age="14 seconds old",
+    continent="South America",
+    vstatus="Fully Vaccincated",
+    petInfo="Lorem ipsum dolor"
+
 }) => {
     return (
         <Container>
-            <Paragraph>
+            <FImg src="https://www.fillmurray.com/500/700" />
+            <SCont>
+                <Paras>
                 {animal}
+                </Paras>
+                <Para>
                 {type}
+                </Para>
+                <Para>
                 {age}
-                {country}
+                </Para>
+                <Para>
+                {continent}
+                </Para>
+                <Para>
                 {vstatus}
-            </Paragraph>
+                </Para>
+            </SCont>
+            <TCont>
+                {petInfo}
+            </TCont>
         </Container>
     )
 }
--- exotico/pages/ibbytest.js
+++ exotico/pages/ibbytest.js
@@ -1,9 +1,17 @@
 import DetailedCard from "../comps/DetailedCard";
 import React from "react";
+import styled from "styled-components";
 
+const MainCont = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+`;
 
 export default function ibbytest() {
     return (
-        <DetailedCard />
+        <MainCont> 
+            <DetailedCard />
+        </MainCont>
     )
 }
