ce7f574227c818604b1a9da0e382b135b4b0785a
Jody Prinsloo <jprinsloo@my.bcit.ca>
2022-01-11 17:30:53 -0800

    initial commit

---
+++ exotico/comps/AnimalCard.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import React from "react";
+
+const CardContainer = styled.div`
+display: flex;
+flex-direction: column;
+width: 150px;
+height: 200px;
+background-color: #a36abb;
+border-radius: 25px;
+`;
+
+const AnimalCardUI = () => {
+    return <CardContainer>
+
+    </CardContainer>
+}
+
+export default AnimalCardUI;
---
+++ exotico/pages/jodyTest.js
@@ -0,0 +1,8 @@
+import AnimalCardUI from "../comps/AnimalCard";
+import React from "react";
+
+export default function jodyTest() {
+    return (
+        <AnimalCardUI />
+    )
+}
