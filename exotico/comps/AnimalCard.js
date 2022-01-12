0eb8a27da1bb705b88146687f09abf91e9b4e71c
Jody Prinsloo <jprinsloo@my.bcit.ca>
2022-01-11 17:40:56 -0800

    initial commit

--- exotico/comps/AnimalCard.js
+++ exotico/comps/AnimalCard.js
@@ -4,15 +4,18 @@ import React from "react";
 const CardContainer = styled.div`
 display: flex;
 flex-direction: column;
-width: 150px;
-height: 200px;
+width: 10%;
+height: 300px;
 background-color: #a36abb;
 border-radius: 25px;
+overflow: hidden;
 `;
+
+const AnimalImg = styled.img``;
 
 const AnimalCardUI = () => {
     return <CardContainer>
-
+        <AnimalImg src="https://placekitten.com/50/50" />
     </CardContainer>
 }
 
